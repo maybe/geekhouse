6ada15ca9013c7d3619d084391302d7677863bce
maybe <suntianwei@msn.com>
2010-02-08 22:48:35 +0800

    delete files, add a new plugin file to implement acl

--- inkmap/library/InkMap/Controller/Plugin/Acl.php
+++ inkmap/library/InkMap/Controller/Plugin/Acl.php
@@ -1,5 +1,5 @@
 <?php
-class CMS_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
+class InkMap_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
 {
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
